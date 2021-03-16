971835804b49c69a666f4360f336df8f7cda83d5
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2021-03-15 20:04:51 -0600

    added root background color in app.css, added styles to aboutus.jsx

--- src/Components/Pages/AboutUs.jsx
+++ src/Components/Pages/AboutUs.jsx
@@ -5,20 +5,24 @@ import "../../Styles/Pages/AboutUs.css";
 function AboutUs() {
   return (
     <section className="text-dark pt-5 pb-5">
-      <div className="container pb-5 ">
+      <div id="cardContainer" className="container pb-5 ">
         <div className="row text-center justify-content-sm-center no-gutters">
           <div className="col-12 col-md-4    ">
             <div className="card shadow-sm h-100">
               <div className=" card-body   p-md-5 ">
                 <img
-                  alt="placeholder"
-                  className="img-fluid d-block mx-auto rounded-circle mt-md-3 mb-4 mt-sm-2"
-                  src="https://via.placeholder.com/100/5fa9f8/efefef"
+                  id="englandFlag"
+                  alt="Flag of England"
+                  className="languageFlags img-fluid d-block mx-auto rounded-circle mt-md-3 mb-4 mt-sm-2"
+                  src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png"
                 />
-                <h2>Feature title</h2>
+                <h2>English</h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse cursus erat sed sem sagittis cursus.
+                  Babel Library is an online language education provider for
+                  students of all ages in Mexico. Are you new around here? We
+                  have qualified Spanish teachers to help you transition into
+                  Mexico life. We also provide English and German to all age
+                  ranges.
                 </p>
                 <p className="mt-4">
                   <a href={Link}>Learn More &gt;</a>
@@ -30,14 +34,17 @@ function AboutUs() {
             <div className="card shadow-sm h-100">
               <div className=" card-body   p-md-5 ">
                 <img
-                  alt="placeholder"
-                  className="img-fluid d-block mx-auto rounded-circle mt-md-3 mb-4 mt-sm-2"
-                  src="https://via.placeholder.com/100/5fa9f8/efefef"
+                  alt="Mexico Flag"
+                  className="languageFlags img-fluid d-block mx-auto rounded-circle mt-md-3 mb-4 mt-sm-2"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1200px-Flag_of_Mexico.svg.png"
                 />
-                <h2>Feature title</h2>
+                <h2>Español</h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse cursus erat sed sem sagittis cursus.
+                  Babel Library es un proveedor de educación de idiomas en línea
+                  para estudiantes de todas las edades en México. ¿Eres nuevo
+                  por aquí? Contamos con profesores de español calificados para
+                  ayudarte en la transición a la vida en México. También
+                  ofrecemos inglés y alemán para todos los rangos de edad.
                 </p>
                 <p className="mt-4">
                   <a href={Link}>Learn More &gt;</a>
@@ -49,17 +56,20 @@ function AboutUs() {
             <div className="card shadow-sm h-100">
               <div className=" card-body   p-md-5 ">
                 <img
-                  alt="placeholder"
-                  className="img-fluid d-block mx-auto rounded-circle mt-md-3 mb-4 mt-sm-2"
-                  src="https://via.placeholder.com/100/5fa9f8/efefef"
+                  alt="Flag of Germany"
+                  className="languageFlags img-fluid d-block mx-auto rounded-circle mt-md-3 mb-4 mt-sm-2"
+                  src="https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg"
                 />
-                <h2>Feature title</h2>
+                <h2>Deutsch</h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse cursus erat sed sem sagittis cursus.
+                  Die Babel Library ist ein Online-Anbieter für Sprachunterricht
+                  für Schüler jeden Alters in Mexiko. Bist du neu hier? Wir
+                  haben qualifizierte Spanischlehrer, die Ihnen beim Übergang in
+                  das mexikanische Leben helfen. Wir bieten auch Englisch und
+                  Deutsch für alle Altersgruppen.
                 </p>
                 <p className="mt-4">
-                  <a href={Link}>Learn More &gt;</a>
+                  <a href={Link}>Learn More</a>
                 </p>
               </div>
             </div>
