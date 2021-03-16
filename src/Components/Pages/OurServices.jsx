e3c1af7699e8a10bdca5e8c72c5713c39054a638
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2021-03-15 21:30:06 -0600

    added styles and html to ourServices.jsx

--- src/Components/Pages/OurServices.jsx
+++ src/Components/Pages/OurServices.jsx
@@ -6,30 +6,36 @@ function OurServices() {
   return (
     <>
       <section className="pt-5 pb-5">
-        <div className="container">
+        <div className=" ourServicesContainer container">
           <div className="row mb-md-4">
             <div className="col-12 col-lg-8 text-center text-lg-left">
-              <h3 className="text-primary small">Lorem ipsum</h3>
-              <h2 className=" ">Lorem ipsum dolor magna</h2>
+              <h3 className="text-primary small">What we do</h3>
+              <h2 className=" ">Our Services To You</h2>
               <p className="lead">
-                Lorem ipsum massa in auctor molestie, magna lorem semper mauris,
-                a venenatis turpis purus vel metus.
+                We provide 100% online synchronous language courses to students
+                of all ages and language levels on our Secure platform.
               </p>
             </div>
           </div>
-          <div className="row d-flex mb-5">
+          <div id="servicesCards" className="row d-flex mb-5">
             <div className="col-10 mx-auto col-md-4">
               <div className="my-3 card card-body shadow p-4 ">
                 <div className="row align-items-center d-flex text-md-center text-lg-left">
                   <div className="col-12 col-sm-3 col-md-3 text-center px-0">
                     <div className="icon-wrap text-primary my-3">
-                      <i className="icon p-4 border-primary  text-primary md-font-size rounded-circle fa fa-tint fa-lg"></i>
+                      <i className="servicesIcon icon p-4 border-primary  text-primary md-font-size rounded-circle fa fa-lock fa-lg">
+                        {"💻"}
+                      </i>
                     </div>
                   </div>
                   <div className="col-12 col-md-9 mt-3 mt-lg-0">
-                    <h4 className="">Lorem ipsum dolor</h4>
+                    <h4 className="">We Zoom</h4>
                     <p className=" mb-0">
-                      Praesent mollis, massa in auctor molestie
+                      Our in-class experience will be with a qualified language
+                      teacher leading a secure zoom classroom. Students will
+                      have both individual and group activities where they can
+                      really use their language skills. Class sessions will be
+                      recorded and posted for student review.
                     </p>
                   </div>
                 </div>
@@ -40,13 +46,20 @@ function OurServices() {
                 <div className="row align-items-center text-md-center text-lg-left">
                   <div className="col-12 col-sm-3 col-md-3 text-center px-0">
                     <div className="icon-wrap text-primary my-3">
-                      <i className="icon p-4 border-primary  text-primary md-font-size rounded-circle fa fa-lock fa-lg"></i>
+                      <i className="servicesIcon icon p-4 border-primary  text-primary md-font-size rounded-circle fa fa-lock fa-lg">
+                        {"📋"}
+                      </i>
                     </div>
                   </div>
                   <div className="col-12 col-md-9 mt-3 mt-lg-0">
-                    <h4 className="">Etiam sed iaculis</h4>
+                    <h4 className="">We Give Homework</h4>
                     <p className="  mb-0">
-                      Praesent mollis, massa in auctor molestie
+                      All reading material will be given to the students through
+                      this platform. Students will use this material to practice
+                      their writing, grammar, and fluency. The homework will be
+                      graded to give the student a sense where their skills land
+                      in that unit. The homework is posted directly to the
+                      student for their privacy. Redos are absolutely fine.
                     </p>
                   </div>
                 </div>
@@ -57,13 +70,19 @@ function OurServices() {
                 <div className="row align-items-center text-md-center text-lg-left">
                   <div className="col-12 col-sm-3 col-md-3 text-center px-0">
                     <div className="icon-wrap text-primary my-3">
-                      <i className="icon p-4 border-primary  text-primary md-font-size rounded-circle fa fa-mobile fa-lg"></i>
+                      <i className="servicesIcon icon p-4 border-primary  text-primary md-font-size rounded-circle fa fa-lock fa-lg">
+                        {"📆"}
+                      </i>
                     </div>
                   </div>
                   <div className="col-12 col-md-9 mt-3 mt-lg-0">
-                    <h4 className="">Auctor molestie</h4>
+                    <h4 className="">Students Are The Same Age</h4>
                     <p className="  sm-font-size text-dark-gray mb-0">
-                      Praesent mollis, massa in auctor molestie
+                      We group our students by both langauge level and age. This
+                      way students will feel more comfortable communicating with
+                      their peers. Adult students and minor students will never
+                      have a class together even if they are at the same
+                      language level.
                     </p>
                   </div>
                 </div>
@@ -76,7 +95,7 @@ function OurServices() {
                 href={Link}
                 className="btn btn-primary  btn-rounded shadow py-3 px-5"
               >
-                Lorem Ipsum
+                Sign Up
               </a>
             </div>
           </div>
