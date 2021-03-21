5c18ede08ad7fa2abd0cdc00c9f5856f8c266ebc
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2021-03-20 18:55:45 -0600

    added contact form styles and background to contact us jsx page

--- src/Components/Pages/ContactUs.jsx
+++ src/Components/Pages/ContactUs.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "../../Styles/Pages/ContactUs.css";
 import ContactUsForm from "../Elements/ContactUsForm.jsx";
+import Background from "../../assets/images/Contact-us.png";
 
 function ContactUs() {
-  return <ContactUsForm />;
+  return (
+    <div
+      className="contactUsPic d-flex container-fluid"
+      style={{ backgroundImage: "url(" + Background + ")" }}
+    >
+      <ContactUsForm />
+    </div>
+  );
 }
 
 export default ContactUs;
