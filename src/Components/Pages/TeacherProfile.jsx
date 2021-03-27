6e8aa3b9a99d95ba3e85665141467d76915a4dcf
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2021-03-26 19:58:19 -0600

    worked on teacher profile and added a google calendar

--- src/Components/Pages/TeacherProfile.jsx
+++ src/Components/Pages/TeacherProfile.jsx
@@ -41,11 +41,24 @@ function TeacherProfile() {
             <div className="mainPage">
               <h3>Lessons Calendar</h3>
               <hr />
-              <img
-                className="calendarPlaceholder"
-                src="https://media.gcflearnfree.org/content/560be4f6557fa909dc9cf5a1_09_30_2015/tips_calendar_intro.jpg"
-                alt=""
-              />
+              <iframe
+                title="lessonCalendar"
+                src="https://calendar.google.com/calendar/embed?src=es.mexican%23holiday%40group.v.calendar.google.com&ctz=America%2FMexico_City"
+                style={{ border: 0 }}
+                width="800"
+                height="600"
+                frameborder="0"
+                scrolling="yes"
+              ></iframe>
+              {/* <iframe
+                title="Kyle Calendar"
+                src="https://calendar.google.com/calendar/embed?src=kyle.pingue%40churchill.edu.mx&ctz=America%2FMexico_City"
+                style={{ border: 0 }}
+                width="800"
+                height="600"
+                frameborder="0"
+                scrolling="no"
+              ></iframe> */}
               <hr />
               <form>
                 <div clasName="input-group mb-3">
@@ -53,43 +66,107 @@ function TeacherProfile() {
                     <label htmlFor="inputGroupSelect01">Select Class</label>
                   </div>
                   <select
-                    className="custom-select"
-                    name=""
+                    className="custom-select options"
+                    name="dropdown"
                     id="inputGroupSelect01"
                   >
                     <option selected>Choose...</option>
-                    <option value="Spanish Beginner ages 6-10"></option>
-                    <option value="Spanish Beginner ages 11-15"></option>
-                    <option value="Spanish Beginner ages 16-18"></option>
-                    <option value="Spanish Beginner ages 19-on"></option>
-                    <option value="Spanish Intermediate ages 6-10"></option>
-                    <option value="Spanish Intermediate ages 11-15"></option>
-                    <option value="Spanish Intermediate ages 16-18"></option>
-                    <option value="Spanish Intermediate ages 19-on"></option>
-                    <option value="Spanish Advanced ages 6-10"></option>
-                    <option value="Spanish Advanced ages 11-15"></option>
-                    <option value="Spanish Advanced ages 16-18"></option>
-                    <option value="Spanish Advanced ages 19-on"></option>
-                    <option value="English Beginner ages 6-10"></option>
-                    <option value="English Beginner ages 11-15"></option>
-                    <option value="English Beginner ages 16-18"></option>
-                    <option value="English Beginner ages 19-on"></option>
-                    <option value="English Intermediate ages 11-15"></option>
-                    <option value="English Intermediate ages 16-18"></option>
-                    <option value="English Intermediate ages 19-on"></option>
-                    <option value="English Advanced ages 11-15"></option>
-                    <option value="English Advanced ages 16-18"></option>
-                    <option value="English Advanced ages 19-on"></option>
-                    <option value="German Beginner ages 6-10"></option>
-                    <option value="German Beginner ages 11-15"></option>
-                    <option value="German Beginner ages 16-18"></option>
-                    <option value="German Beginner ages 19-on"></option>
-                    <option value="German Intermediate ages 11-15"></option>
-                    <option value="German Intermediate ages 16-18"></option>
-                    <option value="German Intermediate ages 19-on"></option>
-                    <option value="German Advanced ages 11-15"></option>
-                    <option value="German Advanced ages 16-18"></option>
-                    <option value="German Advanced ages 19-on"></option>
+                    <option value="Spanish Beginner ages 6-10">
+                      Spanish Beginner ages 6-10
+                    </option>
+                    <option value="Spanish Beginner ages 11-15">
+                      Spanish Beginner ages 11-15
+                    </option>
+                    <option value="Spanish Beginner ages 16-18">
+                      Spanish Beginner ages 16-18
+                    </option>
+                    <option value="Spanish Beginner ages 19-on">
+                      Spanish Beginner ages 19-on
+                    </option>
+                    <option value="Spanish Intermediate ages 6-10">
+                      Spanish Intermediate ages 6-10
+                    </option>
+                    <option value="Spanish Intermediate ages 11-15">
+                      Spanish Intermediate ages 11-15
+                    </option>
+                    <option value="Spanish Intermediate ages 16-18">
+                      Spanish Intermediate ages 16-18
+                    </option>
+                    <option value="Spanish Intermediate ages 19-on">
+                      Spanish Intermediate ages 19-on
+                    </option>
+                    <option value="Spanish Advanced ages 6-10">
+                      Spanish Advanced ages 6-10
+                    </option>
+                    <option value="Spanish Advanced ages 11-15">
+                      Spanish Advanced ages 11-15
+                    </option>
+                    <option value="Spanish Advanced ages 16-18">
+                      Spanish Advanced ages 16-18
+                    </option>
+                    <option value="Spanish Advanced ages 19-on">
+                      Spanish Advanced ages 19-on
+                    </option>
+                    <option value="English Beginner ages 6-10">
+                      English Beginner ages 6-10
+                    </option>
+                    <option value="English Beginner ages 11-15">
+                      English Beginner ages 11-15
+                    </option>
+                    <option value="English Beginner ages 16-18">
+                      English Beginner ages 16-18
+                    </option>
+                    <option value="English Beginner ages 19-on">
+                      English Beginner ages 19-on
+                    </option>
+                    <option value="English Intermediate ages 11-15">
+                      English Intermediate ages 11-15
+                    </option>
+                    <option value="English Intermediate ages 16-18">
+                      English Intermediate ages 16-18
+                    </option>
+                    <option value="English Intermediate ages 19-on">
+                      English Intermediate ages 19-on
+                    </option>
+                    <option value="English Advanced ages 11-15">
+                      English Advanced ages 11-15
+                    </option>
+                    <option value="English Advanced ages 16-18">
+                      English Advanced ages 16-18
+                    </option>
+                    <option value="English Advanced ages 19-on">
+                      English Advanced ages 19-on
+                    </option>
+                    <option value="German Beginner ages 6-10">
+                      German Beginner ages 6-10
+                    </option>
+                    <option value="German Beginner ages 11-15">
+                      German Beginner ages 11-15
+                    </option>
+                    <option value="German Beginner ages 16-18">
+                      German Beginner ages 16-18
+                    </option>
+                    <option value="German Beginner ages 19-on">
+                      German Beginner ages 19-on
+                    </option>
+                    <option value="German Intermediate ages 11-15">
+                      German Intermediate ages 11-15
+                    </option>
+                    <option value="German Intermediate ages 16-18">
+                      German Intermediate ages 16-18
+                    </option>
+                    <option value="German Intermediate ages 19-on">
+                      German Intermediate ages 19-on
+                    </option>
+                    <option value="German Advanced ages 11-15">
+                      German Advanced ages 11-15
+                    </option>
+                    <option value="German Advanced ages 16-18">
+                      German Advanced ages 16-18
+                    </option>
+                    <option value="German Advanced ages 19-on">
+                      German Advanced ages 19-on
+                    </option>
                   </select>
                 </div>
                 <input
