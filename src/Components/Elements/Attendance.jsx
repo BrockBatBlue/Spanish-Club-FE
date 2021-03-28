fea9c894820a2459c2103f4abe38f7bdc448c99a
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2021-03-28 17:52:58 -0600

    added the attendance element and correspondng css

---
+++ src/Components/Elements/Attendance.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Styles/Elements/Attendance.css";
+
+function Attendance() {
+  return (
+    <>
+      <div>This is the attendance element</div>
+    </>
+  );
+}
+
+export default Attendance;
